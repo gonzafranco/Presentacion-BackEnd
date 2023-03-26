1429c604533ad87df0bba0028124beff9787c30c
gonzafranco <gonzafrancoandres@gmail.com>
2023-03-26 20:17:28 -0300

    feat: :sparkles: Libreria Cors y git ignore

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 const sequelize = require('./database/database');
 const usuario = require('./models/usuario');
 const tarea = require('./models/tarea');
@@ -25,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
