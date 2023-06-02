50af65a777a1c7ba1a54504fb9b1b089b8a4dab3
nahuel arias <narias@unpaz.edu.ar>
2023-06-02 16:08:26 -0300

    se resuelve conflicto en auth

--- database/database.js
+++ database/database.js
@@ -29,16 +29,16 @@ const sequelize = new Sequelize("presentacionDB", "plataforma", "plataforma", {
 
 //config casa
 
-const sequelize = new Sequelize('presentacionDB', 'root', 'root', {
-  host: 'localhost',
-  // host: 'postgres', //utilizar cuando se compila el codigo nodejs como container y se utiliza de manera colectiva en docker-compose
-  dialect: 'postgres',
-  port: 5432,
-  define: {
-    // timestamps: true,
-    // freezeTableName: true,
-    paranoid: true
-  }
-});
+// const sequelize = new Sequelize('presentacionDB', 'root', 'root', {
+//   host: 'localhost',
+//   // host: 'postgres', //utilizar cuando se compila el codigo nodejs como container y se utiliza de manera colectiva en docker-compose
+//   dialect: 'postgres',
+//   port: 5432,
+//   define: {
+//     // timestamps: true,
+//     // freezeTableName: true,
+//     paranoid: true
+//   }
+// });
 
 module.exports = sequelize;
